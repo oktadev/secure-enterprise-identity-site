7bf1b8f6928bba519513f151e510d9c4971f99c9
Alisa <7586237+alisaduncan@users.noreply.github.com>
2025-01-03 14:06:19 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Okta Secure Integrations microsite
+
+## Getting going
+
+Fork and clone your forked repo. Run `npm ci` to install dependencies.
+
+Start the server by running `npm start`.
+
+## License
+
+Apache 2.0, see [LICENSE](LICENSE).
---
+++ eleventy.config.js
@@ -0,0 +1,15 @@
+import { HtmlBasePlugin } from '@11ty/eleventy';
+
+export default async function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy({
+			"./public/": "/"
+	});
+
+  eleventyConfig.addPlugin(HtmlBasePlugin);
+};
+
+export const config = {
+    dir: {
+      input: "src"
+    }
+  };
