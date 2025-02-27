90de9d3205d37d02f2955ac72e053914955fa171
Alisa <7586237+alisaduncan@users.noreply.github.com>
2025-02-27 07:16:26 -0800

    fix build with passthroughcopy

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,9 +2,8 @@ import { HtmlBasePlugin } from '@11ty/eleventy';
 
 export default async function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy({
-			"./public/img": "/",
-      "./public/css/index.css": "/"
-	});
+    "./public/": "/"
+  });
 
   eleventyConfig.addCollection("integrations", (collectionsApi) => {
     return collectionsApi.getAll().sort((a,b) => a.data.order > b.data.order)
