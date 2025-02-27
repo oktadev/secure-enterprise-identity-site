966abf8dc9b79a41f8a45765d0ad9eb2d69de321
Alisa <7586237+alisaduncan@users.noreply.github.com>
2025-02-27 06:58:33 -0800

    chore: set up initial content

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,14 +2,19 @@ import { HtmlBasePlugin } from '@11ty/eleventy';
 
 export default async function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy({
-			"./public/": "/"
+			"./public/img": "/",
+      "./public/css/index.css": "/"
 	});
+
+  eleventyConfig.addCollection("integrations", (collectionsApi) => {
+    return collectionsApi.getAll().sort((a,b) => a.data.order > b.data.order)
+  });
 
   eleventyConfig.addPlugin(HtmlBasePlugin);
 };
 
 export const config = {
-    dir: {
-      input: "src"
-    }
-  };
+  dir: {
+    input: "src"
+  }
+};
