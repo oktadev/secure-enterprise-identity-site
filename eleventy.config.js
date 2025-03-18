7d914d72581df07b255b9df4815e96da8d128518
Gabriela Dombrowski <gabrieladombrowski@mac.lan>
2025-03-17 20:31:03 -0500

    Add icon, update button styling, update layout

--- eleventy.config.js
+++ eleventy.config.js
@@ -7,10 +7,14 @@ export default async function(eleventyConfig) {
 
   eleventyConfig.addPlugin(HtmlBasePlugin);
 
-  eleventyConfig.addShortcode("linkButton", function(url, text) {
-    return `<a href={${url}} class="border rounded-sm w-32 p-2 text-center">
+  eleventyConfig.addNunjucksShortcode("linkButton", function(url, text) {
+    return `<a href="${url}" class="border rounded-sm w-32 p-2 text-center hover:border-sii-cyan-light  hover:text-sii-cyan-light ">
               ${text}
             </a>`;
+  });
+
+  eleventyConfig.addNunjucksShortcode("sectionIcon", function(fragment) {
+    return `<img src="img/${fragment}.svg" /> `;
   });
 };
 
