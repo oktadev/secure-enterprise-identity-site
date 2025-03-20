9c26748763c0ab24baccef0c3572bd6606a3706a
Gabi Dombrowski <gabidombrowski@users.noreply.github.com>
2025-03-20 14:37:20 -0500

    UI cleanup (#15)
    * Add shortcode, update styling and layout
    
    * Add icon, update button styling, update layout
    
    * Remove icon logic
    
    * Change padding on xs and add spacing before footer
    
    ---------
    
    Co-authored-by: Gabriela Dombrowski <gabrieladombrowski@mac.lan>
    Co-authored-by: Alisa <7586237+alisaduncan@users.noreply.github.com>

--- eleventy.config.js
+++ eleventy.config.js
@@ -11,8 +11,8 @@ export default async function(eleventyConfig) {
     path: "./public/img",
   });
 
-  eleventyConfig.addShortcode("linkButton", function(url, text) {
-    return `<a href={${url}} class="border rounded-sm w-32 p-2 text-center">
+  eleventyConfig.addNunjucksShortcode("linkButton", function(url, text) {
+    return `<a href="${url}" class="border rounded-sm w-32 p-2 text-center hover:border-sii-med-dark-blue  hover:text-sii-med-dark-blue active:border-sii-cool-dark-blue active:text-sii-cool-dark-blue ">
               ${text}
             </a>`;
   });
