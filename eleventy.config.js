9c46aa0ceda119376c9bb092800e582d96cda92f
Gabi Dombrowski <gabidombrowski@users.noreply.github.com>
2025-03-20 14:29:51 -0500

    Add header icons (#16)
    Co-authored-by: Gabriela Dombrowski <gabrieladombrowski@mac.lan>

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,4 +1,5 @@
 import { HtmlBasePlugin } from '@11ty/eleventy';
+import svgSprite from 'eleventy-plugin-svg-sprite';
 
 export default async function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy({
@@ -6,6 +7,9 @@ export default async function(eleventyConfig) {
   });
 
   eleventyConfig.addPlugin(HtmlBasePlugin);
+  eleventyConfig.addPlugin(svgSprite, {
+    path: "./public/img",
+  });
 
   eleventyConfig.addShortcode("linkButton", function(url, text) {
     return `<a href={${url}} class="border rounded-sm w-32 p-2 text-center">
