545dda7956753bc622cbfc99aa061ad8b2336792
Alisa <7586237+alisaduncan@users.noreply.github.com>
2025-05-08 15:33:31 -0700

    chore: replace header

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,45 +1,46 @@
 import { HtmlBasePlugin } from '@11ty/eleventy'
-import EleventyVitePlugin from "@11ty/eleventy-plugin-vite";
-import tailwindcss from "@tailwindcss/vite";
+import EleventyVitePlugin from '@11ty/eleventy-plugin-vite'
+import tailwindcss from '@tailwindcss/vite'
 
 import shortcodes from './utils/shortcodes.js'
 
 export default function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({
     './public/': '/'
-  });
+  })
 
   eleventyConfig.addPassthroughCopy({
-    "style.out.css": "style.css",
-  });
+    'style.out.css': 'style.css'
+  })
 
-  eleventyConfig.addPlugin(HtmlBasePlugin);
+  eleventyConfig.addPlugin(HtmlBasePlugin)
 
   eleventyConfig.addPlugin(EleventyVitePlugin, {
     viteOptions: {
-      plugins: [tailwindcss()],
-    },
-  });
+      plugins: [tailwindcss()]
+    }
+  })
 
-  eleventyConfig.addPlugin(shortcodes);
-  // Object.keys(shortcodes).forEach((shortcodeName) => {
-	// 	eleventyConfig.addShortcode(shortcodeName, shortcodes[shortcodeName])
-	// });
+  // eleventyConfig.addPlugin(shortcodes);
+  Object.keys(shortcodes).forEach((shortcodeName) => {
+    eleventyConfig.addShortcode(shortcodeName, shortcodes[shortcodeName])
+  })
+
   eleventyConfig.addCollection('topic', function (collectionsApi) {
     return collectionsApi
       .getFilteredByTag('integration')
       .filter((index) => index.data.order < 3)
-  });
+  })
 
-  eleventyConfig.addLayoutAlias('base', 'base.njk');
+  eleventyConfig.addLayoutAlias('base', 'base.njk')
 
   // Per-page bundles, see https://github.com/11ty/eleventy-plugin-bundle
   // Adds the {% css %} paired shortcode
-  eleventyConfig.addBundle("css");
+  eleventyConfig.addBundle('css')
   // Adds the {% js %} paired shortcode
-  eleventyConfig.addBundle("js", {
-    toFileDirectory: "dist",
-  });
+  eleventyConfig.addBundle('js', {
+    toFileDirectory: 'dist'
+  })
 
   return {
     passthroughFileCopy: true,
@@ -47,7 +48,7 @@ export default function (eleventyConfig) {
       input: 'src',
       output: '_site',
       layouts: 'layouts',
-      includes: '_includes',
+      includes: '_includes'
     }
   }
 }
