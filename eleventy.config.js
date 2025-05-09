acbbfd3788467b89d6001b1a5d08d09fe160eae4
Alisa <7586237+alisaduncan@users.noreply.github.com>
2025-05-09 15:18:32 -0500

    chore: refactor site (#57)
    * chore: remove svg sprite and inline theme svgs
    
    * chore: add vite
    
    * chore: refactor base template
    
    * chore: replace header
    
    * chore: remove meta tags from base
    
    * chore: update hero image
    
    * chore: update meta image

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,5 +1,6 @@
 import { HtmlBasePlugin } from '@11ty/eleventy'
-import svgSprite from 'eleventy-plugin-svg-sprite'
+import EleventyVitePlugin from '@11ty/eleventy-plugin-vite'
+import tailwindcss from '@tailwindcss/vite'
 
 import shortcodes from './utils/shortcodes.js'
 
@@ -8,21 +9,47 @@ export default function (eleventyConfig) {
     './public/': '/'
   })
 
-  eleventyConfig.addPlugin(HtmlBasePlugin)
-  eleventyConfig.addPlugin(svgSprite, {
-    path: './public/img'
+  eleventyConfig.addPassthroughCopy({
+    'style.out.css': 'style.css'
   })
 
-  eleventyConfig.addPlugin(shortcodes)
+  eleventyConfig.addPlugin(HtmlBasePlugin)
+
+  eleventyConfig.addPlugin(EleventyVitePlugin, {
+    viteOptions: {
+      plugins: [tailwindcss()]
+    }
+  })
+
+  // eleventyConfig.addPlugin(shortcodes);
+  Object.keys(shortcodes).forEach((shortcodeName) => {
+    eleventyConfig.addShortcode(shortcodeName, shortcodes[shortcodeName])
+  })
+
   eleventyConfig.addCollection('topic', function (collectionsApi) {
     return collectionsApi
       .getFilteredByTag('integration')
       .filter((index) => index.data.order < 3)
   })
 
+  eleventyConfig.addLayoutAlias('base', 'base.njk')
+
+  // Per-page bundles, see https://github.com/11ty/eleventy-plugin-bundle
+  // Adds the {% css %} paired shortcode
+  eleventyConfig.addBundle('css')
+  // Adds the {% js %} paired shortcode
+  eleventyConfig.addBundle('js', {
+    toFileDirectory: 'dist'
+  })
+
   return {
+    passthroughFileCopy: true,
     dir: {
-      input: 'src'
+      input: 'src',
+      output: '_site',
+      layouts: 'layouts',
+      includes: '_includes',
+      data: '_data'
     }
   }
 }
